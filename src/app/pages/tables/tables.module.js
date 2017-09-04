9770a49899280577e3772807ba410b90ccfcbbab
Fabian Pernia <fpernia@velocitypartners.net>
2017-09-04 15:19:20 -0400

    Candidates section and style stuff

--- src/app/pages/tables/tables.module.js
+++ src/app/pages/tables/tables.module.js
@@ -11,31 +11,16 @@
   /** @ngInject */
   function routeConfig($stateProvider, $urlRouterProvider) {
     $stateProvider
-        .state('tables', {
-          url: '/tables',
-          template : '<ui-view  autoscroll="true" autoscroll-body-top></ui-view>',
-          abstract: true,
-          controller: 'TablesPageCtrl',
-          title: 'Tables',
-          sidebarMeta: {
-            icon: 'ion-grid',
-            order: 300,
-          },
-        }).state('tables.basic', {
-          url: '/basic',
-          templateUrl: 'app/pages/tables/basic/tables.html',
-          title: 'Basic Tables',
-          sidebarMeta: {
-            order: 0,
-          },
-        }).state('tables.smart', {
-          url: '/smart',
-          templateUrl: 'app/pages/tables/smart/tables.html',
-          title: 'Smart Tables',
-          sidebarMeta: {
-            order: 100,
-          },
-        });
+      .state('candidates', {
+        url: '/candidates',
+        controller: 'TablesPageCtrl',
+        templateUrl: 'app/pages/tables/smart/tables.html',
+        title: 'Candidates',
+        sidebarMeta: {
+          icon: 'ion-ios-people',
+          order: 100,
+        },
+      });
     $urlRouterProvider.when('/tables','/tables/basic');
   }
 
